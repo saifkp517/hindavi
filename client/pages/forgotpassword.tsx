4b5ecad68e837afb66ad5260676d48841f0f7528
Syed Hammad (starCoder) <syedhammad626@gmail.com>
2022-08-01 20:20:32 +0530

    added forgotpassword file

---
+++ client/pages/forgotpassword.tsx
@@ -0,0 +1,74 @@
+import Container from '@mui/material/Container';
+import {
+  Button,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Typography,
+} from '@mui/material';
+import type { NextPage } from 'next';
+import { Email, Tag } from '@mui/icons-material';
+
+const Verify: NextPage = () => {
+  return (
+    <Container
+      maxWidth='sm'
+      sx={{
+        width: '100%',
+        minHeight: '100vh',
+        display: 'grid',
+        placeContent: 'center',
+      }}
+    >
+      <Typography
+        color='primary'
+        variant='h3'
+        component='h1'
+        sx={{
+          width: '100%',
+          fontWeight: 700,
+        }}
+      >
+        Forgot Password
+      </Typography>
+      <Typography
+        color='primary.light'
+        variant='body1'
+        component='p'
+        marginTop={0.5}
+        sx={{
+          width: '100%',
+          marginLeft: 0.5,
+        }}
+      >
+        Please enter the email address associated with the account
+      </Typography>
+      <form>
+        <FormControl fullWidth={true} variant='outlined' sx={{ marginTop: 4 }}>
+          <InputLabel htmlFor='email'>Email</InputLabel>
+          <OutlinedInput
+            id='email'
+            type='email'
+            label='Email'
+            startAdornment={
+              <InputAdornment position='start'>
+                <Email />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Button
+          type='submit'
+          variant='contained'
+          fullWidth={true}
+          sx={{ fontWeight: '700', paddingY: 1, color: '#fff', marginTop: 4 }}
+        >
+          Finish
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default Verify;
